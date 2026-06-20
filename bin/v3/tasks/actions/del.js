b4daf74c3f8d264dd12d69bd8d3b14cf45f76226
keshavsoft <nknnkumar@yahoo.com>
2026-06-20 09:19:32 +0530

    3 del to test

---
+++ bin/v3/tasks/actions/del.js
@@ -0,0 +1,61 @@
+import path from "path";
+
+import generateRest from "kschema-fs-api-gen-rest";
+import fixEndpointsJs from "express-fix-endpoints-del-js";
+
+import { locateSource } from "./Del/steps/locateSource.js";
+import { locateDestination } from "./Del/steps/locateDestination.js";
+import { createFolder } from "../../core/createFolder.js";
+
+import { announce } from "./Del/steps/announce.js";
+
+import resolveFolderName from "./Del/steps/resolveFolderName.js";
+
+const startFunc = async ({ cmd = "", toPath, isAnnounce = true, checkBeforeCreate = true,
+    inTargetPath, inGenerateRest = false, inFolderName
+}) => {
+
+    const localToPath = toPath;
+
+    const resolvedFolderName = resolveFolderName({
+        name: cmd
+    });
+
+    if (resolvedFolderName.KTF === false) {
+        console.log(resolvedFolderName.KReason);
+
+        return;
+    };
+
+    const source = locateSource();
+    const destination = locateDestination({
+        inResolvedFolderName: resolvedFolderName,
+        toPath: localToPath
+    });
+
+    const createFolderResponse = createFolder({
+        source, destination,
+        isAnnounce, checkBeforeCreate
+    });
+
+    if (createFolderResponse.KTF) {
+        const fromEndPointsJs = await fixEndpointsJs({
+            endPointsJsPath: path.join(localToPath, "end-points.js"),
+            inActionName: cmd, inFolderName, inGetType: "simple"
+        });
+
+        if (inGenerateRest) {
+            generateRest({
+                inTargetPath,
+                toPath: path.join(localToPath, resolvedFolderName),
+                inMethod: "DELETE"
+            });
+        };
+    };
+
+    if (isAnnounce) announce({ inResolvedFolderName: resolvedFolderName });
+
+    return resolvedFolderName;
+};
+
+export default startFunc;
